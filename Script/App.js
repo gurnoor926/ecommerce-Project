50aec960b445e753ca48e2737c9d7c539ffd07c0
Gurnoor Singh <70206041+gurnoor926@users.noreply.github.com>
2026-01-01 17:16:51 +0530

    project completed

--- Script/App.js
+++ Script/App.js
@@ -1,9 +1,9 @@
 // Load navbar and footer
-fetch("navbar.html")
+fetch("../Pages/navbar.html")
     .then(res => res.text())
     .then(data => document.getElementById("header_menu").innerHTML = data);
 
-  fetch("footer.html")
+  fetch("../Pages/footer.html")
     .then(res => res.text())
     .then(data => document.getElementById("footer_section").innerHTML = data);
   // Range input value display
